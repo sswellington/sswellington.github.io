dad25a6eb4e6aac130c715f369f762aab04e34c5
sswellington <wellws@pm.me>
2020-12-14 21:39:27 -0300

    init haruno

--- static/js/index.js
+++ static/js/index.js
@@ -3,8 +3,8 @@
 const base = { 
   data() {
     return {
-      title: 'Haruno',
-      subtitle: 'Sarada'
+      title: 'Wellington Silva',
+      subtitle: 'Mestrando em Modelagem Computacional'
     }
   }
 }
@@ -12,22 +12,29 @@ const base = {
 Vue.createApp(base).mount('#base')
 
 
-const main = {
+const resume = {
   data() {
     return {
-      msg: 'If you think privacy is unimportant for you because you have nothing to hide, you might as well say free speech is unimportant for you because you have nothing useful to say.',
-      paragraph: 'This is paragraph text. Lorem ipsum dolor sit amet consectetur adipisicing elit.'
+      lncc: 'Mestrando em Modelagem Computacional pelo Laboratório Nacional de Computação Científica (LNCC).',
+      futura: 'Pós-graduando em Arquitetura e Infraestrutura de Tecnologia da Informação pela Faculdade Futura do Grupo Educacional Faveni.',
+      uff: 'Licenciado em Ciência da Computação, graduado pela Universidade Federal Fluminense (UFF). ',
+      iff: 'Aprovado no processo seletivo para ingresso no Instituto Federal de Educação, Ciência e Tecnologia Fluminense (IFF campus Campos-Centro) no ano de 2013.',
+      ic: 'Participou do projeto de Iniciação Científica na graduação em Computação.',
+      cbms: 'Obteve artigo premiado como Finalist Paper in TCCLS Best Paper Award - Presented at the 2019 IEEE International Symposium on Computer-based Medical Systems (CBMS), IEEE Technical Commitee on Computational Life Sciences.',
+      sbc: 'Ex-membro da Sociedade Brasileira de Computação (SBC).',
+      anoti: 'Ex-membro do grupo de pesquisa Análise Numérica, Otimização e Tecnologia da Informação (ANOTi).',
+      interesse: 'Interesse na área de Processamento Digital de Imagens e Aprendizagem de Máquina'
     }
   }
 }
 
-Vue.createApp(main).mount('#main')
+Vue.createApp(resume).mount('#resume')
 
 
 const footer = {
   data() {
     return {
-      name: '2020 © Haruno',
+      name: '2021 © Haruno',
       version: 'Version 0.1',
       by: 'sswellington'
     }
